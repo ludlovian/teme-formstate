e56110cc4faf5fcd8418701821e68d0f9cea3791
Alan Layng <alan@readersludlow.com>
2019-06-12 12:47:43 +0100

    Update dependencies

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+import { terser } from 'rollup-plugin-terser'
+import cleanup from 'rollup-plugin-cleanup'
+
+export default {
+  input: 'src/index.js',
+  external: ['teme'],
+  plugins: [
+    cleanup(),
+    process.env.NODE_ENV === 'production' && terser()
+  ],
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.mjs',
+      format: 'esm',
+      sourcemap: true
+    }
+  ]
+}
